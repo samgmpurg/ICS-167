52db691fd1df28ce9e042f846044f5233a2dfb10
samgmpurg <117250903+samgmpurg@users.noreply.github.com>
2023-02-03 15:24:36 -0800

    Pause/Menu Draft Complete

---
+++ ICS-167/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+ 
+    public static bool PausedGame = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PausedGame)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        PausedGame = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        PausedGame = true;
+    }
+
+    public void LoadMenu()
+    {
+        // Unlock in the future if you are going to have animations for the Menu Art! -sam
+        // Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
