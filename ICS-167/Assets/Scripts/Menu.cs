dd24c66453052a8ab8a8e176f13dc35031449646
samgmpurg <117250903+samgmpurg@users.noreply.github.com>
2023-02-03 14:27:54 -0800

    Menu Draft Complete

--- ICS-167/Assets/Scripts/Menu.cs
+++ ICS-167/Assets/Scripts/Menu.cs
@@ -5,15 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Debug.Log("QUIT!");
+        Application.Quit();
     }
 }
