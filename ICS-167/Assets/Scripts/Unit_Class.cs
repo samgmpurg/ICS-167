db6c5744ff48eebf68b42755e355415ca3f99abf
AsadEllis <ellis909@gmail.com>
2023-02-07 13:03:04 -0800

    Setting up Player and Groupable

--- ICS-167/Assets/Scripts/Unit_Class.cs
+++ ICS-167/Assets/Scripts/Unit_Class.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public abstract class UnitInstance : MonoBehaviour
 {
-    [SerializeField]
+    /*[SerializeField]
     private NavMeshAgent agent;
-    public int PlayerNum = 1;
     public void Initalize(string name, SOUnitDefinition.UnitTypes type,Player owner){
         UnitName = name;
         Type = type;
-        agent.SetDestination() //This needs to be finished when we set the spawn position spots of each player
+        agent.SetDestination(); //This needs to be finished when we set the spawn position spots of each player
     }
     //Need to implement a - Remove units funtion, unit Attack function, unit move function(navmesh probably)
+*/
 }
