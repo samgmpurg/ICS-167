7d1322573e7bb3c35efd72d02f8a3f65d0a964c1
AsadEllis <ellis909@gmail.com>
2023-02-21 15:10:36 -0800

    Asad Movement
    Test movement 2/21/2023

--- ICS-167/Assets/Scripts/Unit Scripts/Circle_Unit.cs
+++ ICS-167/Assets/Scripts/Unit Scripts/Circle_Unit.cs
@@ -9,7 +9,7 @@ public class Circle_Unit : UnitInstance
     // Start is called before the first frame update
     void Start()
     {
-       // move = transform.position;
+       move = transform.position;
     }
 
     // Update is called once per frame
--- ICS-167/Assets/Scripts/Unit_Class.cs
+++ ICS-167/Assets/Scripts/Unit_Class.cs
@@ -4,99 +4,44 @@ using UnityEngine;
 
 public abstract class UnitInstance : MonoBehaviour
 {
-
-    //public float moveSpeed = 1.0f;
+    public float moveSpeed = 1f;
     //private Cursor TestCursor;
     //int cursorX = 0;
     //int cursorY = 0;
     //int cursorZ = 0;
-    //public Vector3 move;
-    private bool isMoving;  //if our player is moving or not
-    private Vector3 originalPos, targetPos;
-    private float timeToMove = 0.7f;
-
-    //void Update()
-    //{
-
-    //}
-    public void updateUnitPosition()  //updates the position of the characters based on the keys the playr is pressing
+    public Vector3 move;
+    public bool isMoveable = true;
+    void Update()
     {
-        //if (isMoveable)
-        //{
-        //    if (Input.GetKey("a"))
-        //    {
-        //        if (move.x > 0)
-        //        {
-        //            move.x = move.x - (moveSpeed);
-        //            isMoveable = false;
-        //        }
-        //    }
-        //    if (Input.GetKey("d"))
-        //    {
-        //        if (move.x < 13)
-        //        {
-        //            move.x = move.x - (moveSpeed);
-        //            isMoveable = false;
-        //        }
-        //    }
-        //    if (Input.GetKey("w"))
-        //    {
-        //        if (move.y < 13)
-        //        {
-        //            move.y = move.y + (moveSpeed);
-        //            isMoveable = false;
-        //        }
-        //    }
-        //    if (Input.GetKey("s"))
-        //    {
-        //        if (move.y > 0)
-        //        {
-        //            move.y = move.y - (moveSpeed);
-        //            isMoveable = false;
-        //        }
-        //    }
-
-        //transform.position = move;
-
-        if(Input.GetKey(KeyCode.W) && !isMoving)
-        {
-            StartCoroutine(MovePlayer(Vector3.up));    
-        }
-        if (Input.GetKey(KeyCode.A) && !isMoving)
-        {
-            StartCoroutine(MovePlayer(Vector3.left));
-        }
-        if (Input.GetKey(KeyCode.S) && !isMoving)
-        {
-            StartCoroutine(MovePlayer(Vector3.down));
-        }
-        if (Input.GetKey(KeyCode.D) && !isMoving)
-        {
-            StartCoroutine(MovePlayer(Vector3.right));
-        }
     }
-
-    private IEnumerator MovePlayer(Vector3 direction)
+    public void updateUnitPosition()
     {
-        isMoving = true;
-
-        //keeping track of the elapsed time
-        float elapsedTime = 0;
-
-        originalPos = transform.position;
-        targetPos = originalPos + direction;
-
-        //makes sure we lerp from the original position to the target pos, in the exact time we move
-        while (elapsedTime < timeToMove)
-        {
-            transform.position = Vector3.Lerp(originalPos, targetPos, (elapsedTime / timeToMove)); 
-            elapsedTime += Time.deltaTime;
-            yield return null;
+        if(isMoveable){
+            if(Input.GetKey("a")){
+                if(move.x>0){
+                    move.x=move.x-(moveSpeed);
+                    isMoveable=false;
+                }
+            }
+            if(Input.GetKey("d")){
+                if(move.x<13){
+                    move.x=move.x+(moveSpeed);
+                    isMoveable=false;
+                }
+            }
+            if(Input.GetKey("w")){
+                if(move.y<13){
+                    move.y=move.y+(moveSpeed);
+                    isMoveable=false;
+                }
+            }
+            if(Input.GetKey("s")){
+                if(move.y>0){
+                    move.y=move.y-(moveSpeed);
+                    isMoveable=false;
+                }
+            }
+            transform.position = move;
         }
-
-        transform.position = targetPos;
-
-        isMoving = false;
     }
 }
-    
