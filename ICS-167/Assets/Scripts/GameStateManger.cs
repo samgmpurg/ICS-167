162eb89d6e3b42c6104529ba705220c66e68d168
yeeunm <yeeunm2@uci.edu>
2023-03-01 22:17:44 -0800

    Big update
    -Temporary unit assets are added for a better visual aid when distinguishing units.
    -Kinda modified the enemy class and created 5 different subclasses for our 5 units and I might be modifying it later to meet the inheritance requirement.

--- ICS-167/Assets/Scripts/GameStateManger.cs
+++ ICS-167/Assets/Scripts/GameStateManger.cs
@@ -11,15 +11,19 @@ public class GameStateManger : MonoBehaviour
     public GameObject object3;
     public GameObject object4;
     public GameObject object5;
-    public GameObject object6;
+    public GameObject object6; //enemy1
+    public GameObject enemy2;
+    public GameObject enemy3;
+    public GameObject enemy4;
+    public GameObject enemy5;
     [SerializeField] private GameObject player = null;
     [SerializeField] private GameObject enemy = null;
     [SerializeField] private Slider playerHealth = null;
     [SerializeField] private Slider enemyHealth = null;
     [SerializeField] private Button attackBtn = null;
     [SerializeField] private Button healBtn = null;
-    public GameObject object7;
-    private Vector2 spot7;
+    public GameObject object7; //cursor
+    
     private int playerHP, enemyHP = 100;
 
     private bool isPlayersTurn = true;
@@ -30,7 +34,13 @@ public class GameStateManger : MonoBehaviour
     private Vector3 spot3;
     private Vector3 spot4;
     private Vector3 spot5;
-    private Vector3 spot6;
+    private Vector3 spot6; //spot for enemy1
+    private Vector2 spot7; //spot for cursor
+
+    private Vector3 spot8; //spot for enemy2
+    private Vector3 spot9; //spot for enemy3
+    private Vector3 spot10; //spot for enemy4
+    private Vector3 spot11; //spot for enemy5
 
 
     private static GameStateManger _instance;
@@ -44,6 +54,10 @@ public class GameStateManger : MonoBehaviour
         spot4 = new Vector3(5.0f, 2.0f, 1.0f);
         spot5 = new Vector3(7.0f, 6.0f, 1.0f);
         spot6 = new Vector3(6.0f, 10.0f, 1.0f);
+        spot8 = new Vector3(10.0f, 14.0f, 1.0f);
+        spot9 = new Vector3(8.0f,12.0f, 1.0f);
+        spot10 = new Vector3(4.0f, 12.0f, 1.0f);
+        spot11 = new Vector3(3.0f, 11.0f, 1.0f);
         if (_instance == null)
         {
             _instance = this; //standard GameStateManger stuff
@@ -57,6 +71,10 @@ public class GameStateManger : MonoBehaviour
             Destroy(object5);
             Destroy(object6);
             Destroy(object7);
+            Destroy(enemy2);
+            Destroy(enemy3);
+            Destroy(enemy4);
+            Destroy(enemy5);
         }//This is a very bad way to initialize stuff and WILL be changed in the fututre
         DontDestroyOnLoad(object1);
         Instantiate(object1, spot1, Quaternion.identity); //Spawns everything in
@@ -72,6 +90,14 @@ public class GameStateManger : MonoBehaviour
         Instantiate(object6, spot6, Quaternion.identity);
         DontDestroyOnLoad(object7);
         Instantiate(object7,spot7, Quaternion.identity);
+        DontDestroyOnLoad(enemy2);
+        Instantiate(enemy2, spot8, Quaternion.identity);
+        DontDestroyOnLoad(enemy3);
+        Instantiate(enemy3, spot9, Quaternion.identity);
+        DontDestroyOnLoad(enemy4);
+        Instantiate(enemy4, spot10, Quaternion.identity);
+        DontDestroyOnLoad(enemy5);
+        Instantiate(enemy5, spot11, Quaternion.identity);
     }
 
     //want to know who we want to attack and how much dmg they will take
--- ICS-167/Assets/Scripts/Unit Scripts/Enemy.cs
+++ ICS-167/Assets/Scripts/Unit Scripts/Enemy.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    protected int mov;
+    [SerializeField]
+    protected int atkRange;
+    [SerializeField]
+    private Vector3 location {get; set;}
+    [SerializeField]
+    private GameObject redSquare;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,28 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void defineAtkTile()
+    {
+        ArrayList temp = new ArrayList();
+
+        for ( int i = (int)(location.y) - mov; i <= location.y + mov; i++)
+        {
+            for (int j = (int)(location.x) - mov; j <= location.x + mov; j++)
+            {
+                if (Mathf.Abs(i + j) <= mov)
+                    temp.Add(new Vector3(i, j));
+            }
+        }
+
+        for ( int i = 0; i < temp.Count; i++)
+        {
+            foreach (Vector3 vec in temp)
+                Instantiate(redSquare, vec, Quaternion.identity);
+        }
+
+    }
+
 }
